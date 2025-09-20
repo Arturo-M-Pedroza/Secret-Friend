89b80cdf8f1e8b2c874d93aade089c9cb35131ca
Arturo Morales <arturo.m.pedroza@gmail.com[200~git commit -m "added a new file browser-support.text"git config --global user.email arturo.m.pedroza@gmail.com>
2025-09-19 21:53:45 -0600

    first version

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,36 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let friends = []; 
+
+function agregarAmigo() {
+    let name = document.getElementById("amigo").value;
+    if (name.trim() !== "") {
+        friends.push(name);
+        document.getElementById("amigo").value = "";
+        showFriends();
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+}
+
+function showFriends() {
+    let list = document.getElementById("listaAmigos");
+    list.innerHTML = "";
+    for (let i = 0; i < friends.length; i++) {
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(friends[i]));
+        list.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (friends.length === 0) {
+        alert("No hay amigos en la lista.");
+        return;
+    } else {
+        let randomIndex = Math.floor(Math.random() * friends.length) + 1;
+        randomFriend = friends[randomIndex];
+        document.getElementById("resultado").innerHTML = "Tu amigo seleccionado es: " + randomFriend;
+
+    }
+}
